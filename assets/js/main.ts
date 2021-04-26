c7febd6e7c91fc319936c85aa7861af49e1a4686
Tomasz Krawczyk <grubach1@gmail.com>
2021-04-26 13:04:21 +0200

    display error in outpu area

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,17 +1,17 @@
 // @ts-ignore
 import UbirchVerification from './node_modules/ubirch-verification-js/src/verification';
 // @ts-ignore
-import { FormUtils } from './node_modules/ubirch-verification-js/src/form-utils/form-utils';
+// import { FormUtils } from './node_modules/ubirch-verification-js/src/form-utils/form-utils';
 import {
   EHashAlgorithms,
   EStages,
   // @ts-ignore
 } from './node_modules/ubirch-verification-js/src/models/models';
 
-const formUtils = new FormUtils({ formIds: ['created', 'name', 'workshop'] });
-const params = FormUtils.getFormParamsFromUrl(window);
-formUtils.setDataIntoForm(params, document);
-console.log(params);
+// const formUtils = new FormUtils({ formIds: ['created', 'name', 'workshop'] });
+// const params = FormUtils.getFormParamsFromUrl(window);
+// formUtils.setDataIntoForm(params, document);
+
 let ubirchVerification: UbirchVerification | null = null;
 
 // verify JSON button click listener
@@ -52,31 +52,25 @@ document.getElementById('verify-json').addEventListener('click', function () {
     return;
   }
 
-  try {
-    const hash = ubirchVerification.createHash(json);
-    ubirchVerification
-      .verifyHash(hash)
-      .then((response) => {
-        const msg =
-          'Verification success!!\n\nVerificationResult:\n' +
-          response.upp +
-          '\n' +
-          response.upp.state +
-          '\n' +
-          response.verificationState;
-        (document.getElementById('output') as HTMLInputElement).value = msg;
-      })
-      .catch((errResponse) => {
-        const msg =
-          'Verification failed!!\n\nErrorResponse:\n' +
-          errResponse.verificationState +
-          ', ' +
-          errResponse.failReason;
-        window.alert(msg);
-      });
-  } catch (e) {
-    // handle the error yourself and inform user about the missing fields
-    const msg = 'JSON Verification failed!\n';
-    window.alert(msg);
-  }
+  const hash = ubirchVerification.createHash(json);
+  ubirchVerification
+    .verifyHash(hash)
+    .then((response) => {
+      const msg =
+        'Verification success!!\n\nVerificationResult:\n' +
+        JSON.stringify(response.upp) +
+        '\n' +
+        response.upp.state +
+        '\n' +
+        response.verificationState;
+      (document.getElementById('output') as HTMLInputElement).value = msg;
+    })
+    .catch((errResponse) => {
+      const msg =
+        'Verification failed!!\n\nErrorResponse:\n' +
+        errResponse.verificationState +
+        ', ' +
+        errResponse.failReason;
+      (document.getElementById('output') as HTMLInputElement).value = msg;
+    });
 });
