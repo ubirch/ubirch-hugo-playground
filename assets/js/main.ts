e3ad1fb281797a73aaa7d09962e756b24ccc998c
Tomasz Krawczyk <grubach1@gmail.com>
2021-05-20 17:35:32 +0200

    all schemas and handling specials

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -11,13 +11,12 @@ const { EHashAlgorithms, EStages } = models;
 
 const formUtils = new UbirchFormUtils();
 
-const params = formUtils.getFormParamsFromUrl(window, ';');
-formUtils.setDataIntoForm(params, document);
+let params = formUtils.getFormParamsFromUrl(window, ';');
 
 (document.getElementById('json-input') as HTMLInputElement).value =
   JSON.stringify(params);
 
-  let schemaName = '';
+let schemaName = '';
 let ubirchVerification: UbirchVerification | null = null;
 let ubirchVerificationWidget: UbirchVerificationWidget | null = null;
 let subscribe = null;
@@ -46,8 +45,8 @@ const initToken = (accessToken: string) => {
   });
 
   if (!subscribe)
-    subscribe = ubirchVerification.messenger.subscribe((q: any) =>
-      console.log(q)
+    subscribe = ubirchVerification.messenger.subscribe((msg: any) =>
+      console.log(msg)
     );
 };
 
@@ -60,6 +59,15 @@ document.addEventListener('DOMContentLoaded', () => {
       const value = JSON.parse((radio as HTMLInputElement).value);
       initToken(value.token);
       schemaName = value['data_schema'];
+
+      params = formUtils.getFormParamsFromUrl(window, ';');
+      const parsedParams = handleSpecials(params, schemaName);
+      const sortedParams = ubirchVerification.formatJSON(
+        JSON.stringify(parsedParams)
+      );
+
+      (document.getElementById('json-input') as HTMLInputElement).value =
+        sortedParams;
     });
   });
 });
@@ -85,11 +93,68 @@ document.getElementById('verify-json').addEventListener('click', function () {
   }
 
   validate(schemaName, json)
-    .then(() => {
-      const hash = ubirchVerification.createHash(json);
+    .then((validated) => {
+      const stringified = JSON.stringify(validated);
+      const hash = ubirchVerification.createHash(stringified);
       ubirchVerification.verifyHash(hash);
     })
     .catch((err) => {
       showInfo(`${err.name}: ${err.errors?.join(', ')}`);
     });
 });
+
+function handleSpecials(json: any, DATA_SCHEMA: string) {
+  switch (DATA_SCHEMA) {
+    case 'DATA_SCHEMA_certification-vaccination-v3':
+      let vacc = {
+        da: json.da,
+        vp: json.vp,
+        pr: json.pr,
+        br: json.br,
+        vs: json.vs,
+      } as any;
+      if (json.bn) {
+        vacc.bn = json.bn;
+      }
+      if (json.vd) {
+        vacc.vd = json.vd;
+      }
+      if (json.ac) {
+        vacc.ac = json.ac;
+      }
+      if (json.di) {
+        vacc.di = json.di;
+      }
+      if (json.co) {
+        vacc.co = json.co;
+      }
+      if (json.nx) {
+        vacc.nx = json.nx;
+      }
+      let vaccV3Json = {
+        fn: json.fn,
+        id: json.id,
+        is: json.is,
+        ve: json.ve,
+        vaccination: [vacc],
+      } as any;
+      if (json.gn) {
+        vaccV3Json.gn = json.gn;
+      }
+      if (json.bd) {
+        vaccV3Json.bd = json.bd;
+      }
+      if (json.pn) {
+        vaccV3Json.pn = json.pn;
+      }
+      if (json.vf) {
+        vaccV3Json.vf = json.vf;
+      }
+      if (json.vu) {
+        vaccV3Json.vu = json.vu;
+      }
+      return vaccV3Json;
+    default:
+      return json;
+  }
+}
--- assets/js/validation.ts
+++ assets/js/validation.ts
@@ -12,6 +12,42 @@ const schemas = {
     r: yup.string().required(),
     s: yup.string().required(),
   }),
+  'DATA_SCHEMA_certification-bvdw-certificate': yup.object().shape({
+    co: yup.string().required(),
+    do: yup.string().required(),
+    vf: yup.string().required(),
+    vu: yup.string().required(),
+  }),
+  'DATA_SCHEMA_certification-vaccination': yup.object().shape({}),
+  'DATA_SCHEMA_certification-vaccination-v3': yup.object().shape({
+    fn: yup.string().required(),
+    id: yup.string().required(),
+    is: yup.string().required(),
+    ve: yup.string().required(),
+    vaccination: yup
+      .array()
+      .length(1)
+      .of(
+        yup.object().shape({
+          da: yup.string().required(),
+          vp: yup.string().required(),
+          pr: yup.string().required(),
+          br: yup.string().required(),
+          vs: yup.string().required(),
+          bn: yup.string(),
+          vd: yup.string(),
+          ac: yup.string(),
+          di: yup.string(),
+          co: yup.string(),
+          nx: yup.string(),
+        })
+      ),
+    gn: yup.string(),
+    bd: yup.string(),
+    pn: yup.string(),
+    vf: yup.string(),
+    vu: yup.string(),
+  }),
 };
 
 const validate = (schemaName: string, json: any) => {
@@ -21,7 +57,7 @@ const validate = (schemaName: string, json: any) => {
       name: 'ValidationError',
       errors: ['Schema not found'],
     });
-  return schemas[schemaName].validate(json);
+  return schemas[schemaName].validate(json, { stripUnknown: true });
 };
 
 export default validate;
